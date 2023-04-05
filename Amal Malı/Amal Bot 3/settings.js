c1fc124e3cc925297c5cf7f3bf20a55c2d2d9cc8
ravgarcık <97904458+ravgar@users.noreply.github.com>
2023-04-05 20:07:53 +0300

    Add files via upload

---
+++ Amal Malı/Amal Bot 3/settings.js
@@ -0,0 +1,60 @@
+const settings = {
+	"MongoDB": "mongodb+srv://Solomon:Ma.626409636@cluster0.2bqt4.mongodb.net/chaos-moderation?retryWrites=true&w=majority",
+    "commandChannel": ["956581275999817759"],
+    "taglıAlım": false,
+    "onsekizatilacakoda": "",
+    "onsekizodalar": [],
+    "sorunCozucu": [],
+    "terapist": [],
+    "basvuruLog": "",
+    "chatMesajı": "-member-, Aramıza hoş geldiniz! Rol seçim odalarından rolleriniz almayı unutmayın iyi eğlenceler.",
+
+    "TAG_SYSTEM_CHANNEL": "956581275999817759",
+    "YETKI_VER_LOG": "956581275999817759",
+    "CEZA_PUAN_KANAL": "956581275999817759",
+    "CEZA_PUAN_SYSTEM": true,
+
+    // PUAN SİSTEMİ KURULUM
+    "CHAT_KANAL": "956581310363750473",
+    "PUBLIC_KATEGORI": "956581283721527359",
+    "STREAMER_KATEGORI": "956581285197914142",
+    "REGISTER_KATEGORI": "956581277086150687",
+    "SLEEP_ROOM": "956581309310980126",
+
+    // AUTO UPDATE KURULUM
+    "GörevSystem": {
+        "KanalID": "",
+        "MesajID": ""
+    },
+
+    "leaderboard": {
+        "KanalID": "",
+        "mesajID": "",
+        "mesajID_2": "",
+        "mesajID_3": "",
+        "mesajID_4": "",
+    },
+
+    "yetkilisay": {
+        "KanalID": "",
+        "MesajID": ""
+    },
+
+
+
+    "MODERASYON": "OTYwNTY0ODAzNjQwOTE0MDM4.YksRrw.JWQHICfwgmZohp-4loxHY4yV2NY", // mod botu
+    "STATS": "OTYwNTY0OTYzMTk2NDI4MzQ4.YksR1Q.c4K7Ex97FoV6wLbGzJ598_LRslQ", // stat
+    "EXECUTIVE": "OTYwNTY1MDU5MTI4NTM3MTI5.YksR7A._xoZCBxzzkfMkCryKRj--0taxZ8", // yönetim
+    "LOG": "OTYwNTY1MzEyODI5NDE1NTI0.YksSKQ.jCd1L2_6qnpE-N33WLhpgtDUZJI", // log botu
+    "ASYNC": "OTYxMzI5OTQwNTYzOTE4OTM5.Yk3aRg.-1aDMDnoMxkvZCkTnCtZey9bnss", // async botu
+
+    "prefix": [".","!","m!"],
+    "botSesID": "956581294920335360",
+    "sunucuId": "864187021312131072",
+    "sahip": ["852814638889828372"],
+    "footer": "Hz.Meriaz",
+    "readyFooter": ["M E R I A Z", "Bot işleri için `Meriaz#3488"]
+}
+
+
+module.exports = settings;
